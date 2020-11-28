89c6810a8832d20f3ca987974824da7fac6e0ba2
zhbink <zhbink@163.com>
2020-11-28 21:58:20 +0800

    创建mall-mbg

---
+++ mall-mbg/src/main/java/com/micro/mall/mapper/UserMapper.java
@@ -0,0 +1,7 @@
+package com.micro.mall.mapper;
+
+import com.micro.mall.model.User;
+import tk.mybatis.mapper.common.Mapper;
+
+public interface UserMapper extends Mapper<User> {
+}
---
+++ mall-mbg/src/main/java/com/micro/mall/model/User.java
@@ -0,0 +1,38 @@
+package com.micro.mall.model;
+
+import java.util.Date;
+import javax.persistence.*;
+
+import lombok.Data;
+import lombok.experimental.Accessors;
+
+@Data
+@Accessors(chain = true)
+@Table(name = "user")
+public class User {
+    @Id
+    @GeneratedValue(generator = "JDBC")
+    private Integer id;
+
+    @Column(name = "open_id")
+    private String openId;
+
+    @Column(name = "nick_name")
+    private String nickName;
+
+    @Column(name = "avatar_url")
+    private String avatarUrl;
+
+    private Integer gender;
+
+    private String country;
+
+    private String province;
+
+    private String city;
+
+    private String language;
+
+    @Column(name = "register_time")
+    private Date registerTime;
+}
